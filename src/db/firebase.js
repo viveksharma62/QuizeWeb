064bb50cb7c159f80c69c3915eade5cb3568568a
viveksharma62 <viveksharma28402@gmail.com>
2025-09-12 21:44:13 +0530

    update

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -3,15 +3,36 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
+
+
+// src/firebase.js
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBlvejFrP_Tw9HMLn9PaN0hYg8i0cWcyr0",
+//   authDomain: "quizeweb-d92e8.firebaseapp.com",
+//   projectId: "quizeweb-d92e8",
+//   storageBucket: "quizeweb-d92e8.firebasestorage.app",
+//   messagingSenderId: "93919331965",
+//   appId: "1:93919331965:web:b3b0acf43a04a21c4f5314",
+//   measurementId: "G-5776SY11DX"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+// export const auth = getAuth(app);
